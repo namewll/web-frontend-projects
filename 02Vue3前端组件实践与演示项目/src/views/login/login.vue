58ef695cb50a6d2f2aef2823ee9f7de41fc8b40d
namewll <3147423800@qq.com>
2026-03-29 13:40:12 +0800

    Update login.vue

--- 02Vue3前端组件实践与演示项目/src/views/login/login.vue
+++ 02Vue3前端组件实践与演示项目/src/views/login/login.vue
@@ -36,12 +36,12 @@
                rules:{
                     name:
                         [ 
-                            { required: true, message: 'Please input Activity name', trigger: 'blur' },
+                            { required: true, message: 'Please input name', trigger: 'blur' },
                             { min: 3, max: 5, message: 'Length should be 3 to 5', trigger: 'blur' }
                         ],
                     password:
                         [ 
-                            { required: true, message: 'Please input Activity name', trigger: 'blur' },
+                            { required: true, message: 'Please input password', trigger: 'blur' },
                             { min: 7, max: 16, message: 'Length should be 7 to 16', trigger: 'blur' }
                         ]
                }
